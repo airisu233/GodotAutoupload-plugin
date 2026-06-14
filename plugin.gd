ba0f898cbba8b8b9b1edd107c78667ae00531084
airisu233 <98138727+airisu233@users.noreply.github.com>
2026-06-15 05:08:33 +0800

    Update plugin.gd

--- plugin.gd
+++ plugin.gd
@@ -2,34 +2,48 @@
 extends EditorPlugin
 
 const BACKUP_BAT := "res://addons/GithubAutoUPload/github.bat"
-const BACKUP_COOLDOWN := 30.0
-
+const BACKUP_COOLDOWN := 60.0
+var is_exiting := false
 var last_backup_time := 0
 
 func _enter_tree() -> void:
 	resource_saved.connect(_on_resource_saved)
+	print("GithubAutoUpload plugin loaded")
 
 func _exit_tree() -> void:
+	is_exiting = true
 	resource_saved.disconnect(_on_resource_saved)
+	print("Plugin unloaded")
+		# 退出前强制备份一次
+	_run_backup()
+	print("Plugin unloaded")
 
 func _on_resource_saved(resource: Resource) -> void:
 	var path := resource.resource_path
-	
-	if not (path.ends_with(".tscn") or path.ends_with(".gd") or path.ends_with(".tres")):
-		return
+	print("Resource saved: ", path)
 	
 	var current_time := Time.get_ticks_msec() / 1000
-	if current_time - last_backup_time < BACKUP_COOLDOWN:
+	var time_diff := current_time - last_backup_time
+	
+	print("Time since last backup: ", time_diff, "s")
+	
+	if not is_exiting and time_diff < BACKUP_COOLDOWN:
+		print("Backup skipped, cooldown: ", BACKUP_COOLDOWN - time_diff, "s remaining")
 		return
 	
 	last_backup_time = current_time
+	
+	print("Starting backup for: ", path)
 	_run_backup()
 
 func _run_backup() -> void:
 	var bat_path := ProjectSettings.globalize_path(BACKUP_BAT)
+	print("Executing: ", bat_path)
+	
+	# 用 create_process 真正异步，不阻塞
 	var pid := OS.create_process("cmd", ["/c", bat_path])
 	
 	if pid == -1:
-		print("Backup failed")
+		print("Failed to start backup process")
 	else:
-		print("Backup started, PID: ", pid)
+		print("Backup process started, PID: ", pid)
